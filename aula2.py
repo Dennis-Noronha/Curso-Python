2e9c9ec74ed1f1fbf12665f6cb04ee74c68a19a2
Dennis Noronha <dennis@Note.Linux>
2026-05-08 21:50:23 -0300

    Aula 2 de Python

---
+++ aula2.py
@@ -0,0 +1,6 @@
+# Aula 2 - Função print()
+""" A função print() é usada para exibir informações na tela. 
+Ela pode receber vários argumentos e tem opções para personalizar a saída. """
+# \r\n -> CRLF (Carriage Return + Line Feed) -> Nova linha
+print(12, 34, sep='-')
+print(56, 78, sep='-')
