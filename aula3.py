5511cd247d6bc27250f2c022c94040d210b63503
Dennis Noronha <dennis@Note.Linux>
2026-05-08 22:21:44 -0300

    Aula 3 de Python

---
+++ aula3.py
@@ -0,0 +1,23 @@
+"""
+DocString 
+Python = Linguagem de Programação
+Tipo de tipagem = Dinâmica / Forte
+str -> String -> Texto 
+Strings são textos que estão dentro de aspas.
+"""
+
+print (12345)
+
+# Aspas simples
+print('Dênnis Noronha!')
+print(1, 'Dênnis o "Noronha!" ')
+
+# Aspas duplas
+print("Dênnis Noronha!")
+print(2, "Dênnis o 'Noronha!' ")
+
+# Escape 
+print("Dênnis \"Noronha!\"")
+
+#r
+print(r"Dênnis \"Noronha!\"")
