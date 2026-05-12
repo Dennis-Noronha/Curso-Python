19cc12efee947ef7e3587e36a8e690f2cec7c63d
Dennis Noronha <dennis@Note.Linux>
2026-05-11 22:24:14 -0300

    Aula 4 de python

---
+++ aula4.py
@@ -0,0 +1,17 @@
+# Tipos int e float  
+# int: números inteiros, sem parte decimal
+# float: números reais, com parte decimal
+# int sem sinal é considerado positivo, mas pode ser negativo.
+# Exemplos de int
+print(10)  # int positivo
+print(-5)  # int negativo 
+print(0)   # int zero    
+
+# Exemplos de float
+print(3.14, 10.11)   # float positivo
+print(-2.5)   # float negativo
+print(0.0)    # float zero  
+
+# A função type() é usada para verificar o tipo de uma variável ou valor
+print(type(1.1))
+print(type(10))
