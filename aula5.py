c4cbffcb3a72af7798f438955af7297fa12fc086
Dennis Noronha <dennis@Note.Linux>
2026-05-11 22:48:07 -0300

    Aula 5 de Python

---
+++ aula5.py
@@ -0,0 +1,17 @@
+# Tipo de dado bool (boolean)  
+# Um valor booleano pode ser True (verdadeiro) ou False (falso)
+# Existem vários operadores para "questionar" um valor booleano, como:
+# == (igual a)
+# != (diferente de)
+# > (maior que)
+# < (menor que)
+# >= (maior ou igual a)
+# <= (menor ou igual a) 
+
+# Exemplo de uso do tipo bool
+print(10 == 10)  # True
+print(10 == 11)  # False
+print(5 > 3)     # True
+print(5 < 3)     # False
+print(5 >= 5)    # True
+print(5 <= 4)    # False        
