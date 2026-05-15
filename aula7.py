cfe5092ca475ace2a79d6dbb7f4ba680f322b6f1
Dennis Noronha <dennis@Note.Linux>
2026-05-15 19:47:19 -0300

    Aula 7 de Python

---
+++ aula7.py
@@ -0,0 +1,11 @@
+# Introdução a variaveis 
+# Variáveis são usadas para armazenar dados e valores em um programa.
+# PEP8: Inicie o nome de uma variável com letra minúscula, 
+# e use letras minúsculas e underscores para separar palavras (snake_case).
+# O sinal de = é usado para atribuir um valor a uma variável.
+# Exemplo de uso de variáveis
+idade = 20
+nome = "Dênnis Noronha"
+altura = 1.75
+maior_de_idade = idade >= 18
+print("Nome:", nome, "Idade:", idade, "Altura:", altura, "Maior de idade:", maior_de_idade)
