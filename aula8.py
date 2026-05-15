90c761c512e46aca3b4cd3c17e1c05827f9f5de7
Dennis Noronha <dennis@Note.Linux>
2026-05-15 20:35:18 -0300

    Aula 8/Exercício em Python

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = 'Dênnis'
+sobrenome = 'Noronha'
+idade = 20
+ano_nascimento = 2026  -idade
+maior_de_idade = idade >= 18
+altura = 1.65  
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('é maior de idade?:', maior_de_idade)
+print('Altura:', altura)
